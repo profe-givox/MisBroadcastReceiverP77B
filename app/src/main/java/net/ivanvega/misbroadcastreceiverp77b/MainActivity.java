7dca50ecd169cdd8bdf2fa572871dc9346fd9062
profe-givox <gi.vega@itsur.edu.mx>
2020-02-10 12:27:06 -0600

    Initial commit

---
+++ app/src/main/java/net/ivanvega/misbroadcastreceiverp77b/MainActivity.java
@@ -0,0 +1,14 @@
+package net.ivanvega.misbroadcastreceiverp77b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
