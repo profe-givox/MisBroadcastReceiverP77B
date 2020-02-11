5008c8b446568d4cd2131d83a7b8d3dedbbc97f2
profe-givox <gi.vega@itsur.edu.mx>
2020-02-11 14:12:16 -0600

    Se emitio difusiones desde la misma App

--- app/src/main/java/net/ivanvega/misbroadcastreceiverp77b/MainActivity.java
+++ app/src/main/java/net/ivanvega/misbroadcastreceiverp77b/MainActivity.java
@@ -2,13 +2,43 @@ package net.ivanvega.misbroadcastreceiverp77b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MiReceiverPersonalizado miReceiverPersonalizado =
+            new MiReceiverPersonalizado();
+
+    IntentFilter intentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        intentFilter.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
+        intentFilter.addAction("com.example.broadcast.MY_NOTIFICATION");
+
+        registerReceiver(miReceiverPersonalizado, intentFilter);
+
+    }
+
+    public void btnSend_click(View view) {
+        Intent intent = new Intent();
+        intent.setAction("com.example.broadcast.MY_NOTIFICATION");
+        intent.putExtra("data","Notice me senpai!");
+        sendBroadcast(intent);
     }
 }
--- app/src/main/java/net/ivanvega/misbroadcastreceiverp77b/MiReceiverPersonalizado.java
+++ app/src/main/java/net/ivanvega/misbroadcastreceiverp77b/MiReceiverPersonalizado.java
@@ -3,6 +3,7 @@ package net.ivanvega.misbroadcastreceiverp77b;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -11,6 +12,30 @@ public class MiReceiverPersonalizado extends BroadcastReceiver {
     private static final String TAG = "MyBroadcastReceiver";
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        if(intent.
+            getAction()
+            .equals
+            (TelephonyManager.ACTION_PHONE_STATE_CHANGED)){
+            Toast.
+            makeText
+                    (context,
+                            "ENtro al IF",
+                            Toast.LENGTH_SHORT).show();
+
+        }
+        if(intent.
+                getAction()
+                .equals
+                        ("com.example.broadcast.MY_NOTIFICATION")){
+            Toast.
+                    makeText
+                            (context,
+                                    "Mi difusion ",
+                                    Toast.LENGTH_SHORT).show();
+
+        }
+
         StringBuilder sb = new StringBuilder();
         sb.append("Action: " + intent.getAction() + "\n");
         sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
